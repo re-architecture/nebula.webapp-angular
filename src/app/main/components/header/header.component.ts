0215d1e891b68e9829b3680d35fdd51150407268
sunxiaofeng <sunxiaofeng@msn.com>
2018-10-09 11:32:22 +0800

    add application custom style

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { LoginService, Principal } from 'src/app/nebula-core';
 
 @Component({
   selector: 'main-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    private principal: Principal
+  ) { }
 
   ngOnInit() {
   }
 
+  logout() {
+
+    this.loginService.logout();
+
+  }
+
+  isAuthenticated() {
+    return this.principal.isAuthenticated();
+}
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,18 +4,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { DefaultPageComponent } from './pages/default-page/default-page.component';
 import { MainRoutingModule } from './main-routing.module';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
-import { MaterialModule } from 'src/app/shared';
+import { MaterialModule, FontAwesomeModule } from 'src/app/shared';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FontAwesomeModule,
   ],
   declarations: [
     HeaderComponent, 
     DefaultPageComponent,
-    DefaultLayoutComponent
+    DefaultLayoutComponent,
+    TopMenuComponent
   ]
 })
 export class MainModule { }
